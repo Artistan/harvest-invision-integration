f23213a3da3bd6a8074d76eca73d1318752c2a06
Charles Peterson <artistan@gmail.com>
2017-08-29 14:25:16 -0500

    Still have CSP issues..

--- harvest_git_integration.user.js
+++ harvest_git_integration.user.js
@@ -5,8 +5,10 @@
 // @description  Harvest button integration with Git* pages
 // @author       InfiniteRed
 // @source       https://github.com/Artistan/harvest-scripts-integration//blob/master/harvest_git_integration.user.js
-// @include      https://*git*/issue*
-// @require      http://code.jquery.com/jquery-1.12.4.min.js
+// @include      http://*git*/*
+// @include      https://*git*/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+// @require      https://platform.harvestapp.com/assets/platform.js
 // @grant        none
 // ==/UserScript==
 
@@ -16,41 +18,31 @@ var IV = {};
 
 IV.pageState = {
     currentHash: '',
-    onWorkflowScreen: false,
-    prototypeId: '',
-    taskId: '',
+    UserGroup: '',
+    Repo: '1',
+    action: '1',
+    itemId: '1',
     harvestIsReady: false
 };
 
 IV.selectors = {
-    workflowTitle: ".workflow-screen-titlebar__screen-title",
     harvestInvisionTimer: "#harvest-invision-timer",
     assignment: ".btn:contains('Comment'),.btn:contains('Close'),.btn:contains('New'),.btn:contains('Edit')",
     harvestMessaging: "#harvest-messaging"
 };
 
 IV.setStateFromHash = function () {
-    IV.pageState.currentHash = window.location.hash;
-    var hashArray = window.location.hash.split('/');
-    /**
-    console.log(hashArray[0]); // #
-    console.log(hashArray[1]); // projects
-    console.log(hashArray[2]); // prototypes
-    console.log(hashArray[3]); // prototype_id
-    console.log(hashArray[4]); // workflow
-    console.log(hashArray[5]); // undefined or screen
-    console.log(hashArray[6]); // undefined or task_id
-    **/
-    IV.pageState.onWorkflowScreen = hashArray[5] === 'screen';
-    IV.pageState.prototypeId = hashArray[3] || '';
-    IV.pageState.taskId = hashArray[6] || '';
+    IV.pageState.currentHash = window.location.pathname;
+    var hashArray = window.location.pathname.split('/');
+    IV.pageState.UserGroup = hashArray[1];
+    IV.pageState.Repo = hashArray[2];
+    IV.pageState.action = hashArray[3];
+    IV.pageState.itemId = hashArray.pop();
 };
 
 IV.handlePageState = function () {
    IV.setStateFromHash();
-   if (IV.isOnWorkflowScreen()) {
-       IV.injectPlaceholderButton();
-   }
+   IV.injectPlaceholderButton();
 };
 
 IV.getPlatformConfig = function () {
@@ -64,45 +56,28 @@ IV.getPlatformConfig = function () {
 IV.initialize = function () {
     // inject Harvest platform config script
     var harvestPlatformConfigScript = 'window._harvestPlatformConfig = ' + JSON.stringify(IV.getPlatformConfig()) + ';';
-    $('<script>')
+    jQuery('<script>')
     .attr('type', 'text/javascript')
     .text(harvestPlatformConfigScript)
     .appendTo('head');
 
-    // inject Harvest platform script
-    $('<script>')
-    .attr('type', 'text/javascript')
-    .attr('async', true)
-    .attr('src', 'https://platform.harvestapp.com/assets/platform.js')
-    .appendTo('head');
-
    IV.handlePageState();
-};
-
-IV.isOnWorkflowScreen = function () {
-    return IV.pageState.onWorkflowScreen === true;
 };
 
 IV.onHashChange = function () {
-   IV.pageState.onWorkflowScreen = false;
    IV.handlePageState();
 };
 
-IV.getWorkflowTitle = function () {
-    var txt = $(IV.selectors.workflowTitle).text();
-    return txt.trim();
-};
-
 IV.buildTimer = function () {
-   var timer = $('<div/>', {
+   var timer = jQuery('<div/>', {
     "id": "harvest-invision-timer",
     "class" : "harvest-timer",
-    "data-item": '{"id": ' + IV.pageState.taskId + ', "name": "' + 'Git* Screen: ' + IV.getWorkflowTitle() + '"}',
-    "data-account": '{"id": ' + IV.pageState.prototypeId + '}',
+    "data-item": '{"id": ' + IV.pageState.itemId + ', "name": "' + 'Git*: ' + IV.pageState.Repo + ' ' + IV.pageState.action + '"}',
+    "data-account": '{"id": ' + IV.pageState.UserGroup + '}',
     "style": "float: left; margin-top: 10px; margin-right: 10px;"
    });
-   $(IV.selectors.assignment).parent('div').append(timer);
-   if ($(IV.selectors.harvestInvisionTimer).length) {
+   jQuery(IV.selectors.assignment).parent('div').append(timer);
+   if (jQuery(IV.selectors.harvestInvisionTimer).length) {
       try {
           var event = new CustomEvent("harvest-event:timers:add", {
               detail: { element: document.querySelector(IV.selectors.harvestInvisionTimer) }
@@ -119,7 +94,12 @@ IV.buildTimer = function () {
 };
 
 IV.isReadyToInjectTimer = function () {
-    return $(IV.selectors.assignment).parent('div').length && IV.pageState.harvestIsReady && IV.getWorkflowTitle().length > 0 && $(IV.selectors.harvestMessaging).length;
+    console.log(
+         'IV.selectors.assignment',jQuery(IV.selectors.assignment).parent('div').length,
+         'HarvestPlatform.origin',typeof HarvestPlatform.origin === "string",
+         'IV.selectors.harvestMessaging',jQuery(IV.selectors.harvestMessaging).length
+    );
+    return jQuery(IV.selectors.assignment).parent('div').length && typeof HarvestPlatform.origin === "string" && jQuery(IV.selectors.harvestMessaging).length;
 };
 
 IV.whenReadyForTimer = function(callback) {
@@ -128,9 +108,10 @@ IV.whenReadyForTimer = function(callback) {
         return function() {
             if (!IV.isReadyToInjectTimer()) {
                 return;
+            } else {
+                window.clearInterval(_this.interval);
+                callback();
             }
-            window.clearInterval(_this.interval);
-            return callback();
         };
     })(this);
     window.clearInterval(this.interval);
@@ -141,7 +122,7 @@ IV.injectPlaceholderButton = function () {
     IV.whenReadyForTimer(IV.buildTimer);
 };
 
-$(document).ready(function (){
+jQuery(document).ready(function (){
     'use strict';
     IV.initialize();
 
@@ -153,7 +134,8 @@ $(document).ready(function (){
     }
     setInterval(TrackHash, 200);
 
-    $("body").on("harvest-event:ready", function () {
+    jQuery("body").on("harvest-event:ready", function () {
         IV.pageState.harvestIsReady = true;
     });
 });
+
